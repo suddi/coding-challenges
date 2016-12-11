575cf411b3abb9ec31cac9640b33b9d217f7712f
Sudharshan Ravindran <suddir@gmail.com>
2016-12-11 19:06:15 +0800

    Added comments

--- src/queues/stack_queue.py
+++ src/queues/stack_queue.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-def prepare_dequeue(A):
-    queue = []
-    for i in xrange(0, len(A)):
-        queue.append(A.pop())
-    return queue
+def prepare_dequeue(A):                                             # O(N)
+    queue = []                                                      # O(1)
+    for i in xrange(0, len(A)):                                     # O(N)
+        queue.append(A.pop())                                       # O(1)
+    return queue                                                    # O(1)
 
-def solution(operations):
+def solution(operations):                                           # O(N^2)
     """
     Create a FIFO queue using stack operations only.
 
@@ -14,16 +14,16 @@ def solution(operations):
     1
     3
     """
-    enqueue = []
-    dequeue = []
+    enqueue = []                                                    # O(1)
+    dequeue = []                                                    # O(1)
 
-    for operation in operations:
-        if operation[0] == 'enqueue' and operation[1]:
-            enqueue.append(operation[1])
-        elif operation[0] == 'dequeue':
-            if not dequeue:
-                dequeue = prepare_dequeue(enqueue)
-            print dequeue.pop()
+    for operation in operations:                                    # O(N)
+        if operation[0] == 'enqueue' and operation[1]:              # O(1)
+            enqueue.append(operation[1])                            # O(1)
+        elif operation[0] == 'dequeue':                             # O(1)
+            if not dequeue:                                         # O(1)
+                dequeue = prepare_dequeue(enqueue)                  # O(N)
+            print dequeue.pop()                                     # O(1)
 
 if __name__ == '__main__':
     import doctest
