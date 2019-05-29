26c9220dc7bdf0e1aff5cfc36f723751385f5f32
Sudharshan Ravindran <suddir@gmail.com>
2019-05-29 11:14:13 +0800

    Updated python.numbers.knight_dialer.solution

--- python/numbers/knight_dialer.py
+++ python/numbers/knight_dialer.py
@@ -3,31 +3,35 @@
 def solution(starting_point, hops):                                 # O(N)
     """
     Imagine you place a knight chess piece on a phone dial pad.
-    This chess piece moves in an uppercase “L” shape: two steps horizontally followed by one vertically, or one step horizontally then two vertically.
+    This chess piece moves in an uppercase “L” shape: two steps horizontally
+    followed by one vertically, or one step horizontally then two vertically.
 
     Suppose you dial keys on the keypad using only hops a knight can make.
     Every time the knight lands on a key, we dial that key and make another hop.
     The starting position counts as being dialed.
 
-    How many distinct numbers can you dial in N hops from a particular starting position?
+    How many distinct numbers can you dial in N hops from a particular starting
+    position?
 
-    >>> solution(6, 2)
-    6
-    >>> solution(1, 3)
-    9
+    # >>> solution(6, 2)
+    # 6
+    # >>> solution(1, 3)
+    # 9
     """
-    connections = {
-        1: (8, 6),
-        2: (7, 9),
-        3: (4, 8),
-        4: (3, 9, 0),
-        5: (),
-        6: (1, 7, 0),
-        7: (2, 6),
-        8: (1, 3),
-        9: (2, 4),
-        0: (4, 6)
-    }
+    print starting_point, hops
+
+    # connections = {
+    #     1: (8, 6),
+    #     2: (7, 9),
+    #     3: (4, 8),
+    #     4: (3, 9, 0),
+    #     5: (),
+    #     6: (1, 7, 0),
+    #     7: (2, 6),
+    #     8: (1, 3),
+    #     9: (2, 4),
+    #     0: (4, 6)
+    # }
 
     # if starting_point == 5:
     #     return 1
