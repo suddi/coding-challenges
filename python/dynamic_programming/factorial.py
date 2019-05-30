7e9ee454d34b6dc53f1c672068bbf160745a37fd
Sudharshan Ravindran <suddir@gmail.com>
2019-05-30 10:12:38 +0200

    Updated some challenges

---
+++ python/dynamic_programming/factorial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+def solution(number):                                               # O(N)
+    """
+    Write a function to calculate the factorial of a number
+
+    >>> solution(0)
+    1
+    >>> solution(1)
+    1
+    >>> solution(6)
+    720
+    """
+    m = {
+        0: 1,
+        1: 1
+    }                                                               # O(1)
+
+    for i in xrange(2, number + 1):                                 # O(N)
+        m[i] = i * m[i - 1]                                         # O(1)
+
+    return m[number]                                                # O(1)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
